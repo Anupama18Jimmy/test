654cf97d64f13a0d1a680802edba44e72d87b766
Anupama18 <anupamajimmy18@cet.ac.in>
2020-11-10 23:16:26 +0000

    Component Part2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 // import logo from './logo.svg';
+import { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 // import './App.css';
+import {DISHES} from './shared/dishes.js';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      dishes:DISHES
+    };
+  }
+  render(){
   return (
     <div>
         <Navbar dark color="primary">
@@ -11,12 +20,12 @@ function App() {
             <NavbarBrand href="/">AIRLI</NavbarBrand>
           </div>
         </Navbar>
-
-        {/* ------------------------------------------- */}
-        <Menu />
+{/* ------------------------------------------- */}
+        <Menu dishes={this.state.dishes} />
       </div>
 
-  );
+  );}
 }
 
 export default App;
+
